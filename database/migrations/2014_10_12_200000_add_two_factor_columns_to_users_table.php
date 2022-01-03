ea208d9cfa4f58d5ee5c8140c94b06ddda8bf9d3
MFR Holdings <rick@mfrholdings.com>
2022-01-03 17:37:28 -0500

    Initial commit

---
+++ database/migrations/2014_10_12_200000_add_two_factor_columns_to_users_table.php
@@ -0,0 +1,40 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddTwoFactorColumnsToUsersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        \Illuminate\Support\Facades\DB::statement('SET SESSION sql_require_primary_key=0');
+
+        Schema::table('users', function (Blueprint $table) {
+            $table->text('two_factor_secret')
+                    ->after('password')
+                    ->nullable();
+
+            $table->text('two_factor_recovery_codes')
+                    ->after('two_factor_secret')
+                    ->nullable();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('users', function (Blueprint $table) {
+            $table->dropColumn('two_factor_secret', 'two_factor_recovery_codes');
+        });
+    }
+}
